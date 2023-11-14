2326340dde5d70b99ad85ce87f06391702e5fcc3
Mortarco <mortarco@denasu.com>
2023-11-14 16:47:50 +0900

    init

---
+++ PublicPJ/src/test/java/com/denasu/sample/jetty/service/HelloWorldServiceTest.java
@@ -0,0 +1,15 @@
+package com.denasu.sample.jetty.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldServiceTest
+{
+	@Test
+    public void checkMessage() throws Exception
+    {
+		HelloWorldService service = new HelloWorldService();
+		assertThat(service.getHelloMessage()).isEqualTo("Test Message");
+    }
+}
